df3bfabb161d3b933d07bb32dc93ad7ffdbcce17
Raphael Neumann <darophi@gmail.com>
2022-12-23 21:25:47 +0100

    Inter font. Better structure

--- components/problem.tsx
+++ components/problem.tsx
@@ -3,32 +3,38 @@ import { useState } from "react";
 
 type ProblemProps = {
   problemHeader: string;
-  description: string;
+  description: string[];
   solution: string;
   point: number;
   example?: string[];
+  id: string;
   problem?: string;
   exampleImage?: React.ReactNode,
   problemImage?: React.ReactNode,
 }
 
 export function Problem(props: ProblemProps) {
-
   const [input, setInput] = useState("");
-
   const [isCorrect, setIsCorrect] = useState(false);
 
   const handleSubmit = (e: any) => {
     setIsCorrect(input == props.solution)
-
     e.preventDefault();
   }
 
   return (
-    <div className="pb-[600px] flex flex-col space-y-2">
-      <div className="text-4xl text-red-700">{props.problemHeader}</div>
-      <p>{props.point} point</p>
-      <p>{props.description}</p>
+    <div className="pb-[500px] flex flex-col space-y-2" id={props.id}>
+      <div className="flex space-x-4 items-end ">
+        <div className="text-4xl text-red-700 underline">{props.problemHeader}</div>
+        <p className="text-gray-700 text-2xl font-bold">{props.point} point</p>
+      </div>
+      {props.description &&
+        <div>
+          <p className="font-bold">Eksempel</p>
+          {props.description.map(x => (
+            <div key={x}>{x}</div>
+          ))}
+        </div>}
 
       {props.example &&
         <div>
@@ -56,7 +62,7 @@ export function Problem(props: ProblemProps) {
       </>}
 
 
-      <form onSubmit={handleSubmit} className="flex space-x-2">
+      <form onSubmit={handleSubmit} className="flex space-x-2 items-center">
         <input className="border rounded p-1" value={input} onChange={(e) => setInput(e.target.value)} />
         <button type="submit" className="bg-green-600 px-2 py-1 rounded text-white">Svar</button>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,27 @@ import { Problem } from '../components/problem'
 import Problem4Example from '../components/problem4Example'
 import Problem4 from '../components/problem4'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 const data = [
   {
-    description: "Julemanden har glemt hvad koden er til hans slæde. Uden denne er han ikke i stand til at levere julegaver til alle børn i verden. Han husker blot den rækkefølge af tal han skulle gange sammen for at få koden, men hans lommeregner er gået i stykker, så han kan desværre ikke lave regnestykket. Kan i regne ud hvad koden er?",
+    id: "problem1",
+    description: [
+      "Julemanden har glemt hvad koden er til hans slæde. Uden denne er han ikke i stand til at levere julegaver til alle børn i verden. Han husker blot den rækkefølge af tal han skulle løse for at få koden, men hans lommeregner er gået i stykker, så han kan desværre ikke lave regnestykket. Kan du regne svaret ud for ham?"
+    ],
     problemHeader: "Problem 1",
     solution: "3417",
     point: 5,
     problem: "13 x 7 x 42+ 6 + 9 - 420"
   },
   {
-    description: `
-    I har nu været i stand til at hjælpe ham ind i slæden og afsted i luften med hans rensdyr. Nu finder han ud af, at hans GPS ikke virker helt som tiltænkt. Det er nemlig sådan at den korteste rute mellem alle huse han skal forbi, er krypteret og han skal skrive det dekrypteret svar ind for at kunne få adgang til at se de rigtige koordinater. 
-    Det vi ved er følgende: 
-    
-    Givet et stykke tekst, er alle bogstaver flyttet 7 tegn, dvs. bogstav A → H, B → I, C → J, etc. Dette kaldes også for “Caesar Cipher” og er en af de mest simple måder at krypterer og dekryptere beskeder på og er kendt i omtrent 2000 år.`,
+    id: "problem2",
+    description: [
+      "Du har nu været i stand til at hjælpe ham ind i slæden og afsted i luften med hans rensdyr. Nu finder han ud af, at hans GPS ikke virker. Det er nemlig sådan at den korteste rute mellem alle huse han skal forbi, er krypteret og han skal angive et dekrypteret svar, for at kunne få adgang til at se den rigtige rute som han skal bruge. Det vi ved er følgende:",
+
+      "Givet et stykke tekst, er alle bogstaver flyttet 7 tegn, dvs. bogstav A → H, B → I, C → J, etc. Dette kaldes også for “Caesar Cipher” og er en af de mest simple måder at krypterer og dekryptere beskeder på og er kendt i omtrent 2000 år.",
+    ],
     example: [
       "Krypteret:     Zulthuk",
       "Dekrypteret:   Snemand"
@@ -32,16 +37,21 @@ const data = [
     point: 5
   },
   {
-    description: "Nu er det lykkedes os at dekryptere alle koordinater og nu kan vi komme forbi alle huse og aflevere gaverne. Men det skulle nødig være så nem! For julemanden har nu måske spist lidt for meget i løbet af året, så han kan ikke være i skorstenen og dermed kan han ikke komme ind og ligge gaverne. Han begynder at komme op med en idé på hvordan han hurtigst mulig kan slippe af med de kilo igen! Han ved 1kg Julemandsflæsk skal bruge 2500 Watt for at blive forbrændt. ",
+    id: "problem3",
+    description: [
+      "Nu er det lykkedes os at dekryptere alle koordinater og nu kan vi komme forbi alle huse og aflevere gaverne. Men det skulle nødig være så nem! For julemanden har nu måske spist lidt for meget i løbet af året, så han kan ikke være i skorstenen og dermed kan han ikke komme ind og ligge gaverne. Han begynder at komme op med en idé på hvordan han hurtigst mulig kan slippe af med de kilo igen! Han ved 1kg Julemandsflæsk skal bruge 2500 Watt for at blive forbrændt."
+    ],
     problemHeader: "Problem 3",
     solution: "50000",
     problem: "Hvis han løber 100m forbrænder han 10 Watt, hvor mange meter skal han løbe for at forbrænde 2kg så han kan komme ind i skorstenen?",
     point: 5
   },
   {
-    description: `
-    Nu har julemanden endelig lykkedes med at komme til husene, lykkedes med at komme igennem skorstenen og ud igen. Alle gaver er nu afleveret og det har været en lang nat, med lidt for meget motion og stress. GPS’en er løbet tør for strøm så han finder et stykke papir og tegner byerne og afstanden mellem dem som han husker det. For at komme hjem skal han tage den korteste vej (også den hurtigeste vej i det her tilfælde). 
-Han kommer frem til følgende. "A" er hvor han er, og "Z" er hans destination.`,
+    id: "problem4",
+    description: ["Nu har julemanden endelig lykkedes med at komme til husene, lykkedes med at komme igennem skorstenen og ud igen. Alle gaver er nu afleveret og det har været en lang nat, med lidt for meget motion og stress. GPS’en er løbet tør for strøm så han finder et stykke papir og tegner byerne og afstanden mellem dem som han husker det. For at komme hjem skal han tage den korteste vej (også den hurtigeste vej i det her tilfælde).",
+      "",
+      "Han kommer frem til følgende. \"A\" er hvor han er, og \"Z\" er hans destination."
+    ],
     problemHeader: "Problem 4",
     solution: "28",
     point: 7,
@@ -63,19 +73,20 @@ export default function Home() {
         <title>Yulehygge</title>
         <meta name="description" content="Generated by create next app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='p-4 flex pt-12 flex-col space-y-4'>
-        <div className='text-4xl'>Yulehygge</div>
+        <div className='text-6xl'>Yulehygge</div>
         <div className='text-sm'>Datalogisk inspireret jule problemer</div>
         <div>Hejså! I skal hjælpe julemanden med at levere julegaver til alle og så kom hjem igen. Den som får flest point får en lille gave! </div>
         <div>Point tildeles ved første mølle princip, dvs den hurtigste til at løse opgaven, får point for den!</div>
-
-        <Problem {...data[0]} />
-        <Problem {...data[1]} />
-        <Problem {...data[2]} />
-        <Problem {...data[3]} />
-
+        <div className='flex flex-col space-y-4'>
+          <Problem {...data[0]} />
+          <Problem {...data[1]} />
+          <Problem {...data[2]} />
+          <Problem {...data[3]} />
+        </div>
         <div>
           <p className='text-4xl'>Færdig!</p>
 
